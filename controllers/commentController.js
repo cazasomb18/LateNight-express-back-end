99c1edd6011010a8edaab4f0c5b967020dc8fdca
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-05-25 18:18:20 -0500

    successfully rewrote api calls to reflect salient data

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -30,10 +30,10 @@ router.get('/restaurants/:place_id', async (req, res, next) => {
 
 		if (foundRestaurant){
 			// const foundComments = await Comment.find(req.params.id);
-			const foundComments = await Comment.find({_id: req.params.id});
+			const foundComments = await Comment.findById(req.params.id);
 
 			console.log("================");
-			console.log({foundComments: req.body}, " <====== comments found on " + req.params.place_id + " GET restaurants/:place_id route");
+			console.log(/*foundComments, */" <====== comments found on " + req.params.place_id + " GET restaurants/:place_id route");
 			console.log("================");
 			await res.json({
 				status: 200,
@@ -76,7 +76,6 @@ router.put('/restaurants/:place_id/edit/:comment_id', async (req, res, next) => 
 
 		console.log('THESE ARE THE FOUND COMMENTS');
 		// const foundComment = await Comment.find({place_id: req.params.id});
-
 		console.log(foundRestaurant.comments);
 
 		const updatedComment = await Comment.findByIdAndUpdate(req.params.comment_id, req.body, {new: true});
